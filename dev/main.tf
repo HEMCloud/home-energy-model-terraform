edb6db2635f9bb2141ad7e31e06be4387ea95edd
Ashley Green <ashley.green@baringa.com>
2025-01-26 16:40:03 +0000

    remove old Terraform cloud configuration files and add new main.tf files for development and production environments

---
+++ dev/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  cloud {
+
+    organization = "hem-cloud"
+
+    workspaces {
+      name = "dev"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.84.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
