cc2666a91ae83d252d6eeea675adaeb00413864a
Ashley Green <ashley.green@baringa.com>
2025-02-03 16:37:39 +0000

    Add IAM policy to allow Terraform to assume OrganizationAccountAccessRole in the dev account

--- mgt/iam.tf
+++ mgt/iam.tf
@@ -1,8 +1,3 @@
-import {
-    to = aws_iam_role.terraform_assume_dev_admin
-    id = "TerraformAssumeDevAdmin"
-}
-
 resource "aws_iam_role" "terraform_assume_dev_admin" {
   assume_role_policy = jsonencode({
     Statement = [{
@@ -25,4 +20,18 @@ resource "aws_iam_role" "terraform_assume_dev_admin" {
   description          = "Role used by Terraform to assume OrganizationAccountAccessRole in the dev member account"
   max_session_duration = 3600
   name                 = "TerraformAssumeDevAdmin"
+}
+
+resource "aws_iam_policy" "allow_terraform_to_assume_dev_admin" {
+  description = "Used to allow Terraform to assume the OrganizationAccountAccessRole in the dev member account."
+  name        = "AllowTerraformToAssumeDevAccountAdmin"
+  policy = jsonencode({
+    Statement = [{
+      Action   = "sts:AssumeRole"
+      Effect   = "Allow"
+      Resource = "arn:aws:iam::864981718509:role/OrganizationAccountAccessRole"
+      Sid      = "VisualEditor0"
+    }]
+    Version = "2012-10-17"
+  })
 }
