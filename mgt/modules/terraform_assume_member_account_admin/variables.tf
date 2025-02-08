e7dad55f12764708fd130d7c24261ad92055557b
Ashley Green <ashley.green@baringa.com>
2025-02-08 07:37:41 +0000

    Add nullable constraint to HCP organization, project, and workspace variables

--- mgt/modules/terraform_assume_member_account_admin/variables.tf
+++ mgt/modules/terraform_assume_member_account_admin/variables.tf
@@ -2,15 +2,18 @@ variable "hcp_organization" {
   description = "The HCP Terraform organization name"
   type        = string
   default     = "hem-cloud"
+  nullable = false
 }
 
 variable "hcp_project" {
   description = "The HCP Terraform project name"
   type        = string
   default     = "Home-Energy-Model-Cloud"
+  nullable = false
 }
 
 variable "hcp_workspace" {
   description = "The HCP Terraform workspace name"
   type        = string
+  nullable = false
 }
