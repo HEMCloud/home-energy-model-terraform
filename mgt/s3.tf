e268a5ed7d06d5277465b34a16c8c48b4170192a
Ashley Green <ashley.green@baringa.com>
2025-03-18 08:53:31 +0000

    Add permission for GetObjectTagging in S3 bucket policy for build artifacts

--- mgt/s3.tf
+++ mgt/s3.tf
@@ -11,7 +11,7 @@ data "aws_iam_policy_document" "hem_build_artifacts_policy" {
   statement {
     sid       = "AllowCrossAccountGetObject"
     effect    = "Allow"
-    actions   = ["s3:GetObject"]
+    actions   = ["s3:GetObject", "s3:GetObjectTagging"]
     resources = ["${aws_s3_bucket.hem_build_artifacts.arn}/*"]
     principals {
       type        = "AWS"
