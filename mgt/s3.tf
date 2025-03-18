b08979b98d18e1ef7df7288f373c2f0994ca2fc6
Ashley Green <ashley.green@baringa.com>
2025-03-18 08:48:23 +0000

    Add S3 bucket and policy for build artifacts with cross-account access to MGT

---
+++ mgt/s3.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "hem_build_artifacts" {
+  bucket = "hem-build-artifacts"
+}
+
+resource "aws_s3_bucket_policy" "hem_build_artifacts_policy" {
+  bucket = aws_s3_bucket.hem_build_artifacts.id
+  policy = data.aws_iam_policy_document.hem_build_artifacts_policy.json
+}
+
+data "aws_iam_policy_document" "hem_build_artifacts_policy" {
+  statement {
+    sid       = "AllowCrossAccountGetObject"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.hem_build_artifacts.arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = [for account_id in local.all_account_ids : "arn:aws:iam::${account_id}:root"]
+    }
+  }
+}
