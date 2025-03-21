09bd083758725b9870d4109229471f54e374e9c8
Ashley Green <ashley.green@baringa.com>
2025-03-21 14:44:23 +0000

    Add DynamoDB Lambda module with streaming and lambda orchestrator

---
+++ dev/dynamo-lambda.tf
@@ -0,0 +1,48 @@
+module "dynamo-lambda" {
+  source         = "terraform-aws-modules/lambda/aws"
+  function_name  = "dynamo-stream-orchestrator"
+  runtime        = "python3.13"
+  handler        = "function.lambda_handler"
+  create_package = false
+  s3_existing_package = {
+    bucket = var.build_artifacts_bucket_name
+    key    = "dynamo-stream-orchestrator-lambda-zips/dea0301a5eb3ba0f4ab77a6a4723b75a"
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "dynamo-stream-orchestrator" {
+  event_source_arn  = module.dynamodb_table.dynamodb_table_stream_arn
+  function_name     = module.dynamo-lambda.lambda_function_name
+  starting_position = "LATEST"
+  depends_on        = [aws_iam_role_policy_attachment.attach_dynamo_stream_policy]
+}
+
+data "aws_iam_policy_document" "allow_dynamo_stream_access" {
+  statement {
+    sid = "AllowDynamoStreamAccess"
+
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:DescribeStream",
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+      "dynamodb:ListStreams",
+    ]
+
+    resources = [
+      module.dynamodb_table.dynamodb_table_stream_arn,
+    ]
+  }
+}
+
+resource "aws_iam_policy" "allow_dynamo_stream_access" {
+  name        = "AllowDynamoStreamAccess"
+  description = "Policy to allow access to DynamoDB streams for hem-model-runs"
+  policy      = data.aws_iam_policy_document.allow_dynamo_stream_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_dynamo_stream_policy" {
+  role       = module.dynamo-lambda.lambda_role_name
+  policy_arn = aws_iam_policy.allow_dynamo_stream_access.arn
+}
--- dev/variables.tf
+++ dev/variables.tf
@@ -3,3 +3,9 @@ variable "dev_account_id" {
   type        = string
   default     = "864981718509"
 }
+
+variable "build_artifacts_bucket_name" {
+  description = "The name of the S3 bucket where build artifacts are stored"
+  type        = string
+  default     = "hem-build-artifacts"
+}
