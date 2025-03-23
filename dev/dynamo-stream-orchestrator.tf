8a37f573be6278cd47c79113270566ffddaab887
Ashley Green <ashley.green@baringa.com>
2025-03-23 18:29:13 +0000

    Add IAM policy to allow Lambda to start Step Function execution

--- dev/dynamo-stream-orchestrator.tf
+++ dev/dynamo-stream-orchestrator.tf
@@ -6,7 +6,10 @@ module "dynamo-lambda" {
   create_package = false
   s3_existing_package = {
     bucket = var.build_artifacts_bucket_name
-    key    = "dynamo-stream-orchestrator-lambda-zips/dea0301a5eb3ba0f4ab77a6a4723b75a"
+    key    = "dynamo-stream-orchestrator-lambda-zips/86e33ea776d0c635d9cfd6560d463d21"
+  }
+  environment_variables = {
+    "STATE_MACHINE_ARN" = module.step_function.state_machine_arn
   }
 }
 
@@ -24,17 +27,14 @@ resource "aws_lambda_event_source_mapping" "dynamo-stream-orchestrator" {
 
 data "aws_iam_policy_document" "allow_dynamo_stream_access" {
   statement {
-    sid = "AllowDynamoStreamAccess"
-
+    sid    = "AllowDynamoStreamAccess"
     effect = "Allow"
-
     actions = [
       "dynamodb:DescribeStream",
       "dynamodb:GetRecords",
       "dynamodb:GetShardIterator",
       "dynamodb:ListStreams",
     ]
-
     resources = [
       module.dynamodb_table.dynamodb_table_stream_arn,
     ]
@@ -50,4 +50,28 @@ resource "aws_iam_policy" "allow_dynamo_stream_access" {
 resource "aws_iam_role_policy_attachment" "attach_dynamo_stream_policy" {
   role       = module.dynamo-lambda.lambda_role_name
   policy_arn = aws_iam_policy.allow_dynamo_stream_access.arn
+}
+
+data "aws_iam_policy_document" "allow_step_function_start_execution" {
+  statement {
+    sid    = "AllowStepFunctionStartExecution"
+    effect = "Allow"
+    actions = [
+      "states:StartExecution",
+    ]
+    resources = [
+      module.step_function.state_machine_arn,
+    ]
+  }
+}
+
+resource "aws_iam_policy" "allow_step_function_start_execution" {
+  name        = "AllowStepFunctionStartExecution"
+  description = "Policy to allow the Lambda to start the Step Function"
+  policy      = data.aws_iam_policy_document.allow_step_function_start_execution.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_step_function_start_execution_policy" {
+  role       = module.dynamo-lambda.lambda_role_name
+  policy_arn = aws_iam_policy.allow_step_function_start_execution.arn
 }
