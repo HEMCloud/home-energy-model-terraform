523e5dd4966e7da4332a805496ebaed1cda2b697
Ashley Green <ashley.green@baringa.com>
2025-03-27 11:46:47 +0000

    Revert "Add bucket in prd to test pipline"
    This reverts commit 10296aba258d44abc6665d8e959b5f1bc4efabd1.

--- prd/s3.tf
+++ prd/s3.tf
@@ -2,7 +2,3 @@ module "hem-model-run-s3" {
   source           = "../hem_modules/hem-model-run-s3"
   env_short_string = var.env_short_string
 }
-
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "i-am-a-unique-bucket-name-123987"
-}
