65be2855d86070509388c65ba10fd3b81f236841
Ashley Green <ashley.green@baringa.com>
2025-03-27 11:08:00 +0000

    Add test resource for dev

--- dev/s3.tf
+++ dev/s3.tf
@@ -2,3 +2,7 @@ module "hem-model-run-s3" {
   source           = "../hem_modules/hem-model-run-s3"
   env_short_string = var.env_short_string
 }
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "i-am-a-unique-bucket-name-1239879"
+}
